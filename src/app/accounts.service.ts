5f8de03861d4f654a4ce73aa94c4e151f76d21e6
Zoechbauer <hans.zoechbauer@aon.at>
2020-03-12 12:43:10 +0100

    data Service added

---
+++ src/app/accounts.service.ts
@@ -0,0 +1,28 @@
+export class AccountsService {
+  accounts = [
+    {
+      name: 'Master Account',
+      status: 'active'
+    },
+    {
+      name: 'Testaccount',
+      status: 'inactive'
+    },
+    {
+      name: 'Hidden Account',
+      status: 'unknown'
+    }
+  ];
+
+  constructor() {}
+
+  addAccount(name: string, status: string) {
+    this.accounts.push({ name: name, status: status });
+    console.log(this.accounts);
+  }
+
+  updateStatus(id: number, status: string) {
+    this.accounts[id].status = status;
+    console.log(this.accounts);
+  }
+}
